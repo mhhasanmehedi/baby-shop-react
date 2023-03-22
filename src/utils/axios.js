048fdab769ea3728dab18af7b7f4585ad21c3e9f
mhhasanmehedi <mmmehedi53@gmail.com>
2023-03-22 10:22:48 +0600

    Update

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  // baseURL: "https://baby-shop-api.vercel.app/",
-  baseURL: "http://localhost:9000",
+  baseURL: "https://baby-shop-api.vercel.app",
+  // baseURL: "http://localhost:9000",
 });
 
 export default axiosInstance;
